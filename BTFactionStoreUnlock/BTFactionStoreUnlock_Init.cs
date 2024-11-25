667513884d09523302cc5ec2b237c046ec66bba2
mcb5637 <28106698+mcb5637@users.noreply.github.com>
2024-11-25 19:13:18 +0100

    Add project files.

---
+++ BTFactionStoreUnlock/BTFactionStoreUnlock_Init.cs
@@ -0,0 +1,19 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTFactionStoreUnlock
+{
+    class BTFactionStoreUnlock_Init
+    {
+        public static void Init(string directory, string settingsJSON)
+        {
+            var harmony = HarmonyInstance.Create("com.github.mcb5637.BTFactionStoreUnlock");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ BTFactionStoreUnlock/StarSystem_HasFactionStore.cs
@@ -0,0 +1,42 @@
+﻿using BattleTech;
+using BattleTech.UI;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTFactionStoreUnlock
+{
+    [HarmonyPatch(typeof(StarSystem), "CanUseFactionStore")]
+    class StarSystem_HasFactionStore
+    {
+        public static bool Prefix(StarSystem __instance, ref bool __result)
+        {
+            if (!__instance.HasFactionStore())
+                return true;
+            __result = true;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(SGNavigationScreen), "GetSystemSpecialIndicator")]
+    class SGNavigationScreen_GetSystemSpecialIndicator
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> ins)
+        {
+            foreach (CodeInstruction c in ins)
+            {
+                if (c.opcode == OpCodes.Callvirt && c.operand.ToString().Contains("IsFactionAlly"))
+                    c.operand = AccessTools.DeclaredMethod(typeof(SGNavigationScreen_GetSystemSpecialIndicator), "RetTrue");
+                yield return c;
+            }
+        }
+        public static bool RetTrue(SimGameState s, FactionValue v, List<string> o)
+        {
+            return true;
+        }
+    }
+}
